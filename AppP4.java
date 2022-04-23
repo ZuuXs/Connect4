5e608f9137f2d0467c4b53ccdb0826001a05bdd1
Zack <amiarzack@mail.com>
2022-04-23 15:08:04 +0200

    game finished

--- AppP4.java
+++ AppP4.java
@@ -2,154 +2,117 @@ import java.util.Scanner;
 
 public class AppP4 {
     public static void main (String[] args) throws InterruptedException{
-        Scanner scanner = new Scanner( System.in );  
-        int playerCount=0;
+        try (Scanner scanner = new Scanner( System.in )) {
+            int playerCount=0;
 
 
-        System.out.print( "Enter the number of rows you want (greater than 3): " );
-        int row = scanner.nextInt();
+            System.out.print( "Enter the number of rows you want (greater than 3): " );
+            int row = scanner.nextInt();
 
-        while(row<4){
-            System.out.print( "Greater than 4 or the game becomes unplayable : " );
-            row = scanner.nextInt();                    
-        }
-
-        System.out.print( "Now, the number of columns you want (greater than 3): " );
-        int col = scanner.nextInt();
-
-        while(col<4){
-            System.out.print( "Greater than 3 or the game becomes unplayable : " );
-            col = scanner.nextInt();                    
-        }
-
-
-        System.out.print( "How many people want to play? " );
-        playerCount = scanner.nextInt();
-
-
-
-        while(playerCount<1){
-            System.out.print( "There must be at least 1 player! \nenter again: " );
-            playerCount = scanner.nextInt();
-        }
-        
-
-
-        scanner.nextLine();
-        if(playerCount>1){
-            Player[] listOfPlayers= new Player[playerCount];
-
-            for(int i=0;i<playerCount;i++){
-                System.out.print("Enter the name of player "+(i+1)+": ");
-                listOfPlayers[i] = new Player (scanner.nextLine(),(i+1));
+            while(row<4){
+                System.out.print( "Greater than 4 or the game becomes unplayable : " );
+                row = scanner.nextInt();                    
             }
 
-            if(playerCount%2==1){
-                System.out.println("The number of players is odd, so "+listOfPlayers[playerCount-1].name+" should choose a player to play with by entering his id. here is the list of players:");
-                System.out.println("ID       Player");
-                for (int i=0;i<playerCount-1;i++){
-                    System.out.println(listOfPlayers[i].id+"        "+listOfPlayers[i].name);
-                }
-                System.out.print("You can also play with the pc by typing 0: ");
-                int opp=scanner.nextInt();
+            System.out.print( "Now, the number of columns you want (greater than 3): " );
+            int col = scanner.nextInt();
 
-                while(opp>playerCount || opp<0){
-                    System.out.print("Invalid ID ! enter again: ");
-                    opp=scanner.nextInt();
+            while(col<4){
+                System.out.print( "Greater than 3 or the game becomes unplayable : " );
+                col = scanner.nextInt();                    
+            }
+
+
+            System.out.print( "How many people want to play? " );
+            playerCount = scanner.nextInt();
+
+
+
+            while(playerCount<1){
+                System.out.print( "There must be at least 1 player! \nenter again: " );
+                playerCount = scanner.nextInt();
+            }
+            
+
+
+            scanner.nextLine();
+            if(playerCount>1){
+                Player[] listOfPlayers= new Player[playerCount];
+
+                for(int i=0;i<playerCount;i++){
+                    System.out.print("Enter the name of player "+(i+1)+": ");
+                    listOfPlayers[i] = new Player (scanner.nextLine(),(i+1));
                 }
 
-                if(opp>=0 && opp<=playerCount){
-                    
-                    for(int i=0;i<playerCount-1;i+=2){
+                if(playerCount%2==1){
+                    System.out.println("The number of players is odd, so "+listOfPlayers[playerCount-1].name+" should choose a player to play with by entering his id. here is the list of players:");
+                    System.out.println("ID       Player");
+                    for (int i=0;i<playerCount-1;i++){
+                        System.out.println(listOfPlayers[i].id+"        "+listOfPlayers[i].name);
+                    }
+                    System.out.print("You can also play with the pc by typing 0: ");
+                    int opp=scanner.nextInt();
+
+                    while(opp>playerCount || opp<0){
+                        System.out.print("Invalid ID ! enter again: ");
+                        opp=scanner.nextInt();
+                    }
+
+                    if(opp>=0 && opp<=playerCount){
+                        
+                        for(int i=0;i<playerCount-1;i+=2){
+                            Thread.sleep(1000);
+                            System.out.println();
+                            System.out.println("                       GAME "+(i+2)/2+"                       ");
+                            System.out.println();
+                            System.out.println("The players of this match will be: "+listOfPlayers[i].name+" VS "+listOfPlayers[i+1].name);
+                            Game game= new Game();
+                            game.launch(row,col,listOfPlayers[i],listOfPlayers[i+1]);
+                            game.lookForNewGame();
+                        }
+
+                        Thread.sleep(1000);
+                        System.out.println();
+                        System.out.println("                       GAME "+(playerCount+1)/2+"                       ");
+                        System.out.println();
+
+                        if(opp>0){
+
+                            System.out.println("The players of this match will be: "+listOfPlayers[playerCount-1].name+" VS "+listOfPlayers[opp-1].name);
+                            
+                            Game lastgame=new Game();
+                            lastgame.launch(row,col,listOfPlayers[playerCount-1],listOfPlayers[opp-1]);
+                            lastgame.lookForNewGame();
+                            lastgame.matchEnd();
+
+                        }if(opp==0){
+                            System.out.println("The players of this match will be: "+listOfPlayers[playerCount-1].name+" VS CPU.");
+                            Game game=new Game();
+                            game.launch1P(row,col,listOfPlayers[playerCount-1]);
+                            game.lookForNewGame();
+                            game.matchEnd();
+                        }
+                    }
+                }else{
+                    Game game= new Game();
+                    for(int i=0;i<playerCount;i+=2){
                         Thread.sleep(1000);
                         System.out.println();
                         System.out.println("                       GAME "+(i+2)/2+"                       ");
                         System.out.println();
                         System.out.println("The players of this match will be: "+listOfPlayers[i].name+" VS "+listOfPlayers[i+1].name);
-                        Game game= new Game();
+                        game= new Game();
                         game.launch(row,col,listOfPlayers[i],listOfPlayers[i+1]);
-                        System.out.println();
-                        System.out.println();
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print("LOOKING FOR THE NEXT GAME!");
-                        Thread.sleep(1000);
-                    }
-
-                    Thread.sleep(1000);
-                    System.out.println();
-                    System.out.println("                       GAME "+(playerCount+1)/2+"                       ");
-                    System.out.println();
-
-                    if(opp>0){
-
-                        System.out.println("The players of this match will be: "+listOfPlayers[playerCount-1].name+" VS "+listOfPlayers[opp-1].name);
-                        
-                        Game lastgame=new Game();
-                        lastgame.launch(row,col,listOfPlayers[playerCount-1],listOfPlayers[opp-1]);
-    
-                        System.out.println();
-                        System.out.println();
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print(".");
-                        Thread.sleep(500);
-                        System.out.print("LOOKING FOR THE NEXT GAME!");
-                        Thread.sleep(1000);
-                        System.out.println();
-                        System.out.println();
-                        System.out.println("No more games found.");
-                        System.out.println();
-                        System.out.println("THANK YOU FOR YOUR PARTICIPATION!");
-                        System.out.println();    
-
-                    }if(opp==0){
-                        System.out.println("The players of this match will be: "+listOfPlayers[playerCount-1].name+" VS CPU.");
-                        Game game=new Game();
-                        game.launch1P(row,col,listOfPlayers[playerCount-1]);
                         game.lookForNewGame();
-                        game.matchEnd();
                     }
+                    game.matchEnd();
                 }
             }else{
-                for(int i=0;i<playerCount;i+=2){
-                    Thread.sleep(1000);
-                    System.out.println();
-                    System.out.println("                       GAME "+(i+2)/2+"                       ");
-                    System.out.println();
-                    System.out.println("The players of this match will be: "+listOfPlayers[i].name+" VS "+listOfPlayers[i+1].name);
-                    Game game= new Game();
-                    game.launch(row,col,listOfPlayers[i],listOfPlayers[i+1]);
-                    System.out.println();
-                    System.out.println();
-                    System.out.print(".");
-                    Thread.sleep(500);
-                    System.out.print(".");
-                    Thread.sleep(500);
-                    System.out.print(".");
-                    Thread.sleep(500);
-                    System.out.print("LOOKING FOR THE NEXT GAME!");
-                    Thread.sleep(1000);
-                }
-                System.out.println();
-                System.out.println();
-                System.out.println("No more games found.");
-                System.out.println();
-                System.out.println("THANK YOU FOR YOUR PARTICIPATION!");
-                System.out.println();
-
+                System.out.print("Enter the name of the player: ");
+                Player player = new Player (scanner.nextLine(),1);
+                Game game=new Game();
+                game.launch1P(row,col,player);
             }
-        }else{
-            System.out.print("Enter the name of the player: ");
-            Player player = new Player (scanner.nextLine(),1);
-            Game game=new Game();
-            game.launch1P(row,col,player);
         }  
     }
 }
--- Game.java
+++ Game.java
@@ -27,30 +27,17 @@ public class Game {
         Thread.sleep(1000);
 
         P4 game= new P4(row, col, p1, p2);
-        Scanner scan= new Scanner(System.in);
-        int c;
+        try (Scanner scan = new Scanner(System.in)) {
+            int c;
 
-        System.out.println(game);
+            System.out.println(game);
+            System.out.println();
 
 
-            while(game.verify()==0 && !full(game)){
-                System.out.printf( p1.name+", which column do you want to play? " );
-                c = scan.nextInt();
-                while(!game.tryPlay(c,p1)){
-                    System.out.println();
-                    System.out.print("Try again: ");
+                while(game.verify()==0 && !full(game)){
+                    System.out.println( p1.name+", which column do you want to play? " );
                     c = scan.nextInt();
-                }
-                System.out.println();
-                System.out.println();
-                System.out.println(game);
-                Thread.sleep(1000);
-
-                if (game.verify()==0 && !full(game)){
-                    System.out.println();
-                    System.out.printf( p2.name+", which column do you want to play? " );
-                    c = scan.nextInt();
-                    while(!game.tryPlay(c,p2)){
+                    while(!game.tryPlay(c,p1)){
                         System.out.println();
                         System.out.print("Try again: ");
                         c = scan.nextInt();
@@ -58,11 +45,26 @@ public class Game {
                     System.out.println();
                     System.out.println();
                     System.out.println(game);
-                    System.out.println();
                     Thread.sleep(1000);
+
+                    if (game.verify()==0 && !full(game)){
+                        System.out.println();
+                        System.out.println( p2.name+", which column do you want to play? " );
+                        c = scan.nextInt();
+                        while(!game.tryPlay(c,p2)){
+                            System.out.println();
+                            System.out.print("Try again: ");
+                            c = scan.nextInt();
+                        }
+                        System.out.println();
+                        System.out.println();
+                        System.out.println(game);
+                        System.out.println();
+                        Thread.sleep(1000);
+                    }
                 }
-            }
-            if(game.verify()==1){
+        }
+        if(game.verify()==1){
                 p1.wins++;
                 System.out.println("!!!!!!!!!!!!!!      "+p1.name+" has won.      !!!!!!!!!!!!!!");
                 return;
@@ -79,7 +81,74 @@ public class Game {
             }
     }
 
-    public void launch1P(int row, int col,Player p){
+    public void launch1P(int row, int col,Player p) throws InterruptedException{
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 3);
+ 
+        Player CPU=new Player("CPU", 0);
+
+        p.giveturn(1);
+        CPU.giveturn(2);
+        
+        System.out.println();
+        Thread.sleep(1000);
+        System.out.println("You start.");
+        System.out.println();
+        Thread.sleep(1000);
+
+        P4 game= new P4(row, col, p, CPU);
+        try (Scanner scan = new Scanner(System.in)) {
+            int c;
+
+            System.out.println(game);
+            System.out.println();
+
+
+                while(game.verify()==0 && !full(game)){
+                    System.out.printf( p.name+", which column do you want to play? " );
+                    c = scan.nextInt();
+                    while(!game.tryPlay(c,p)){
+                        System.out.println();
+                        System.out.print("Try again: ");
+                        c = scan.nextInt();
+                    }
+                    System.out.println();
+                    System.out.println();
+                    System.out.println(game);
+                    Thread.sleep(1000);
+
+                    if (game.verify()==0 && !full(game)){
+                        System.out.println();
+                        System.out.println( CPU.name+", which column do you want to play? " );
+                        System.out.println();
+                        Thread.sleep(1000);
+                        System.out.println("CPU is thinking...");
+                        Thread.sleep(3000);
+                        randomNum = ThreadLocalRandom.current().nextInt(1, col+1);
+                        System.out.println("CPU has played column "+randomNum);
+                        game.tryPlay(randomNum, CPU);
+                        System.out.println();
+                        System.out.println();
+                        System.out.println(game);
+                        System.out.println();
+                        Thread.sleep(1000);
+                    }
+                }
+        }
+        if(game.verify()==1){
+                p.wins++;
+                System.out.println("!!!!!!!!!!!!!!      "+p.name+" has won.      !!!!!!!!!!!!!!");
+                return;
+            }
+            if(game.verify()==2){
+                CPU.wins++;
+                System.out.println("!!!!!!!!!!!!!!      "+CPU.name+" has won.      !!!!!!!!!!!!!!");
+                return;
+            } 
+            if (full(game)){
+                System.out.println("!!!!!!!!!!!!!!      Tie!      !!!!!!!!!!!!!!");
+                System.out.println();
+                return;
+            }
 
     }
 
